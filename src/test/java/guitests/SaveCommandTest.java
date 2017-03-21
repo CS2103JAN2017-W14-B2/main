1ff92d40c24ce4e9058873dac25a55f090dc730e
InfinityFrost <iceazul_awakendragon@hotmail.com>
2017-03-21 18:53:27 +0800

    Update SaveCommandTest.java

--- src/test/java/guitests/SaveCommandTest.java
+++ src/test/java/guitests/SaveCommandTest.java
@@ -12,15 +12,15 @@ public class SaveCommandTest extends TaskBossGuiTest {
     @Test
     public void save() {
         //invalid filepath
-		    commandBox.runCommand("save C://user/desktop/^*+#");
-		    assertResultMessage(SaveCommand.MESSAGE_INVALID_FILEPATH);
+	commandBox.runCommand("save C://user/desktop/^*+#");
+	assertResultMessage(SaveCommand.MESSAGE_INVALID_FILEPATH);
 
         //valid filepath
-		    commandBox.runCommand("save C://user/desktop/taskboss");
-		    assertResultMessage(SaveCommand.MESSAGE_SUCCESS);
+	commandBox.runCommand("save C://user/desktop/taskboss");
+	assertResultMessage(SaveCommand.MESSAGE_SUCCESS);
 
         //invalid command
-		    commandBox.runCommand("saveC://user/desktop/taskboss");
-		    assertResultMessage(Messages.MESSAGE_UNKNOWN_COMMAND);
+	commandBox.runCommand("saveC://user/desktop/taskboss");
+	assertResultMessage(Messages.MESSAGE_UNKNOWN_COMMAND);
     }
 }
