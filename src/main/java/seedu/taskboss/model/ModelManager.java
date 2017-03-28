9c5853bb866a147efbf92fdf10387e9e2b4e0389
InfinityFrost <iceazul_awakendragon@hotmail.com>
2017-03-29 01:07:11 +0800

    Update ModelManager.java

--- src/main/java/seedu/taskboss/model/ModelManager.java
+++ src/main/java/seedu/taskboss/model/ModelManager.java
@@ -89,7 +89,8 @@ public class ModelManager extends ComponentManager implements Model {
 
     //@@author A0138961W
     @Override
-    public synchronized void deleteTask(List<ReadOnlyTask> targets) throws TaskNotFoundException, IllegalValueException {
+    public synchronized void deleteTask(List<ReadOnlyTask> targets) throws TaskNotFoundException, 
+            IllegalValueException {
         taskbossHistory.push(new TaskBoss(this.taskBoss));
 
         for (ReadOnlyTask target: targets) {
