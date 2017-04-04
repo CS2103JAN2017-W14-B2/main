2e394667fb2c08e5d4c393b807e86c8f8c5cbbdc
Tan Wei <tanwei0319@gmail.com>
2017-04-04 15:37:07 +0800

    Fix CommandException bug

--- src/main/java/seedu/taskboss/logic/commands/SaveCommand.java
+++ src/main/java/seedu/taskboss/logic/commands/SaveCommand.java
@@ -3,6 +3,8 @@ package seedu.taskboss.logic.commands;
 import java.io.File;
 import java.io.IOException;
 
+import seedu.taskboss.logic.commands.exceptions.CommandException;
+
 //@@author A0138961W
 /**
  * Saves the data at specific filepath. Creates filepath if it does not exist
@@ -34,18 +36,18 @@ public class SaveCommand extends Command {
     }
 
     @Override
-    public CommandResult execute() {
+    public CommandResult execute() throws CommandException {
         assert storage != null;
 
         File f = new File(filepath);
 
         if (!filepath.contains(SYMBOL_SLASH) || !f.isDirectory()) {
-            return new CommandResult(MESSAGE_INVALID_FILEPATH);
+            throw new CommandException(MESSAGE_INVALID_FILEPATH);
         }
 
         if (filepath.contains(SYMBOL_PLUS) || filepath.contains (SYMBOL_HASH) ||
                 filepath.contains (SYMBOL_CARET) || filepath.contains (SYMBOL_ASTERISK)) {
-            return new CommandResult(MESSAGE_INVALID_FILEPATH);
+            throw new CommandException(MESSAGE_INVALID_FILEPATH);
         }
 
         try {
@@ -53,7 +55,7 @@ public class SaveCommand extends Command {
             model.saveTaskboss();
             return new CommandResult(MESSAGE_SUCCESS);
         } catch (IOException e) {
-            return new CommandResult(MESSAGE_INVALID_FILEPATH);
+            throw new CommandException(MESSAGE_INVALID_FILEPATH);
         }
     }
 }
