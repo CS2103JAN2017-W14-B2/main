48bc1148369d088ec894c9dd1eacef1faf4f03b7
Tan Wei <tanwei0319@gmail.com>
2017-04-10 01:58:11 +0800

    Scroll to corresponding category after renaming

--- src/main/java/seedu/taskboss/commons/core/Messages.java
+++ src/main/java/seedu/taskboss/commons/core/Messages.java
@@ -5,7 +5,8 @@ package seedu.taskboss.commons.core;
  */
 public class Messages {
 
-    public static final String MESSAGE_UNKNOWN_COMMAND = "Unknown command";
+    public static final String MESSAGE_UNKNOWN_COMMAND = "Unknown command. Type 'help' in the command box below"
+            + " to find out more information about TaskBoss features and command usage.";
     public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
     public static final String MESSAGE_INVALID_TASK_DISPLAYED_INDEX = "The task index provided is invalid";
     public static final String MESSAGE_TASKS_LISTED_OVERVIEW = "%1$d tasks listed!";
